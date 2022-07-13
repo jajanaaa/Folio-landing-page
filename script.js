dd346219eaf9ce9936b128fa3236849091b53898
janamanzana <96806525+janamanzana@users.noreply.github.com>
2022-07-14 01:28:05 +0200

    Created navbar

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector(".nav-container");
+const logo = document.querySelector(".logo");
 
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
+  if (navbar.classList.contains("active")) {
+    logo.src = "./images/logo-light.png";
+    logo.style.width = "120px";
+  } else {
+    logo.src = "./images/logo.png";
+  }
 });
