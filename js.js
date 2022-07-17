109b91ba2fe88e975fdbe2a62f7c6b6c4e504d67
janamanzana <96806525+janamanzana@users.noreply.github.com>
2022-07-17 20:15:59 +0200

    Finished Contact page

--- js.js
+++ js.js
@@ -1,1 +1,17 @@
-const contactBefore = contact;
+const contactBefore = document.querySelector(".contact-before");
+const contactAfter = document.querySelector(".contact-after");
+
+const buttonSend = document.querySelector(".btn-send");
+const contactContainer = document.querySelector(".contact-container");
+const message = document.querySelector("textarea");
+
+buttonSend.addEventListener("click", () => {
+  console.log(message);
+  if (message.value.length == 0) {
+    alert("Fields cannot be empty");
+  } else {
+    contactBefore.style.display = "none";
+    contactAfter.style.display = "block";
+    contactContainer.style.height = "100vh";
+  }
+});
--- script.js
+++ script.js
@@ -2,16 +2,28 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector(".nav-container");
 const logo = document.querySelector(".logo");
+const navLinks = document.querySelectorAll(".nav-link");
 
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
+  changeLogo();
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navbar.classList.remove("active");
+    changeLogo();
+  });
+});
+
+const changeLogo = function () {
   if (navbar.classList.contains("active")) {
     logo.src = "./images/logo-light.png";
     logo.style.width = "120px";
   } else {
     logo.src = "./images/logo.png";
   }
-});
+};
 
 // STICKY NAV
 const nav = document.querySelector("nav");
@@ -26,6 +38,7 @@ const stickyNav = function (entries) {
 const navObserver = new IntersectionObserver(stickyNav, {
   root: null,
   treshhold: 0,
+  // rootMargin: "-200px",
 });
 
 navObserver.observe(nav);
